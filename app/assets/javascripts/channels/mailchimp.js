31478223bb6e758f3f419d41264660d01c33b376
Chris Hordijk <hordijk.chris@gmail.com>
2019-08-16 14:50:18 +0200

    script

---
+++ app/assets/javascripts/channels/mailchimp.js
@@ -0,0 +1,17 @@
+// select the target node
+var target = document.getElementById('mce-success-response');
+
+// create an observer instance
+var observer = new MutationObserver(function(mutations) {
+  mutations.forEach(function(mutation) {
+    if (target.innerHTML === "Thank you for subscribing!") {
+      target.innerHTML = "Check your email!";
+    }
+  });
+});
+
+// configuration of the observer:
+var config = { attributes: true, childList: true, characterData: true };
+
+// pass in the target node, as well as the observer options
+observer.observe(target, config);
